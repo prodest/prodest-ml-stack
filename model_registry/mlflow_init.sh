524841a88c3670c72998b9ad98e86efde0e1a54a
Messias <messias077@yahoo.com.br>
2025-06-26 22:06:45 -0300

    Evitar erros ao subir novas versões do MLFLow que tenham o schema do banco de dados alterado

--- model_registry/mlflow_init.sh
+++ model_registry/mlflow_init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mlflow db upgrade sqlite:///mlflow.db
 mlflow server --app-name basic-auth --backend-store-uri sqlite:///mlflow.db --host 0.0.0.0 --default-artifact-root $DEFAULT_ARTIFACT_ROOT
